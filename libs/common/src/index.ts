003bbae6f9dc0ca3e6a20a824dc97643e42676c9
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-06-27 03:30:33 +0300

    refactor: update index.js and packege.json files

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,1 +1,6 @@
-export * from "./logger";
+export * from './database';
+export * from './rmq/rmq.service';
+export * from './rmq/rmq.module';
+export * from './logger/winston.config';
+export * from './logger/winston.interceptor';
+export * from './logger/winston.logger';
